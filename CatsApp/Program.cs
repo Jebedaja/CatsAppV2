0b43580376475a1f098cf8fc50842e93b373d7f4
tomkis4 <129515242+tomkis4@users.noreply.github.com>
2024-02-07 23:12:36 +0100

    Dodano sesje

--- CatsApp/Program.cs
+++ CatsApp/Program.cs
@@ -1,6 +1,10 @@
 using CatsApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +15,22 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddHttpClient(); // Dodaj tę linię
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.ExpireTimeSpan = TimeSpan.FromSeconds(600); // ustawiania czas wygaśnięcia ciasteczka na 10 minut
+    options.Events.OnRedirectToLogin = context =>
+    {
+        // Dodanie skryptu JavaScript, który wyświetli pop-up po wygaśnięciu sesji
+        context.Response.ContentType = "text/html";
+        var script = @"
+            <script>
+                alert('You are not logged in or your session has expired');
+                window.location.href = '/Identity/Account/Login';
+            </script>";
+        return context.Response.WriteAsync(script);
+    };
+});
 
 var app = builder.Build();
 
@@ -28,4 +48,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
